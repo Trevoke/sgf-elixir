8eb5e8d622f10843d84ff5c6ac23f996edc96598
David Newell <beta@alumni.rice.edu>
2016-05-08 17:43:12 -0400

    Implementing the inspect protocal for nodes so that they are written nicely

---
+++ lib/inspect.ex
@@ -0,0 +1,14 @@
+alias Sgf.Node
+
+defimpl Inspect, for: Node do
+  def inspect(%Node{ident_props: props}, _) do
+    props
+      |> Map.keys
+      |> Enum.reduce("", fn(key, acc) ->
+        stringified_vals = Map.get(props, key)
+          |> Enum.reduce("", fn(val, acc) -> acc <> "[" <> val <> "]" end)
+
+        acc <> ";" <> to_string(key) <> stringified_vals
+      end)
+  end
+end
