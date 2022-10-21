f30fa0541903b59ef85f232d6f46fe4628b02ba9
Aldric Giacomoni <trevoke@gmail.com>
2022-10-21 19:05:34 -0400

    prep to publish

--- mix.exs
+++ mix.exs
@@ -28,7 +28,8 @@ defmodule ExSgf.MixProject do
     [
       {:rose_tree, "~> 0.2.0"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
