4ebac99487f982fce7244a211006636925f566fb
Aldric Giacomoni <trevoke@gmail.com>
2022-10-21 19:03:26 -0400

    prepare to publish

--- mix.exs
+++ mix.exs
@@ -4,21 +4,21 @@ defmodule ExSgf.MixProject do
   def project do
     [
       app: :ex_sgf,
-      version: "0.1.0",
+      version: "0.3.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: [
         plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
-      ]
+      ],
+      source_url: "https://github.com/Trevoke/sgf-elixir",
+      homepage_url: "https://github.com/Trevoke/sgf-elixir",
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   # Run "mix help deps" to learn about dependencies.
@@ -27,6 +27,17 @@ defmodule ExSgf.MixProject do
       {:rose_tree, "~> 0.2.0"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
+    ]
+  end
+
+  defp description do
+    "Parsing SGF files."
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/Trevoke/sgf-elixir"}
     ]
   end
 end
